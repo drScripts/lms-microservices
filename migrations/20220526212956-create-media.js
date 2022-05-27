7a0c58c5220605baf5288e9729008735ccf7b4f2
drScripts <nathanael.vd@gmail.com>
2022-05-27 08:40:14 +0700

    Add migration and model media

---
+++ migrations/20220526212956-create-media.js
@@ -0,0 +1,36 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Media", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      path: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Media");
+  },
+};
---
+++ models/media.js
@@ -0,0 +1,34 @@
+"use strict";
+const { Model, Sequelize, DataTypes } = require("sequelize");
+
+/**
+ *
+ * @param {Sequelize} sequelize
+ * @param {DataTypes} DataTypes
+ * @returns
+ */
+module.exports = (sequelize, DataTypes) => {
+  class Media extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Media.init(
+    {
+      path: DataTypes.STRING,
+      description: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      modelName: "Media",
+      timestamps: true,
+      paranoid: true,
+    }
+  );
+  return Media;
+};
